4fd3a5ee5e60fa4821bfead4d335213cfc05f333
Vince Ramces Oliveros <10434746+vinceramcesoliveros@users.noreply.github.com>
2025-04-09 18:04:00 +0800

    fix: unused import

--- lib/src/components/month_view_components.dart
+++ lib/src/components/month_view_components.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file.
 
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 
 import '../calendar_event_data.dart';
